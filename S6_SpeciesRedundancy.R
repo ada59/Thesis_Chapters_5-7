6ccb4b83176d0862c7e4b2714307cd14a101c4a3
Your GitHub Username <your_email@example.com>
2022-09-05 12:18:08 +0100

    Clean and organise S6

--- S6_SpeciesRedundancy.R
+++ S6_SpeciesRedundancy.R
@@ -21,45 +21,63 @@ plot_dir <- "C:/Users/Usuario/Documents/PHD/ThesisChapterMexico_I/TemporalChange
 load(paste0(myd, "/HNC.RData"))     # Conservative Historical Native Assemblage
 load(paste0(myd, "/HNB.RData"))     # Broad Historical Native Assemblage
 load(paste0(myd, "/ContN.RData"))   # Contemporary Native Assemblage
-load(paste0(myd, "/ContE.RData"))   # Contemporary Native Assemblage
-load(paste0(myd, "/ContAll.RData")) # Contemporary Exotic assemblage
+load(paste0(myd, "/ContE.RData"))   # Contemporary Exotic Assemblage
+load(paste0(myd, "/ContAll.RData")) # Contemporary Native + Exotic assemblage
 load(paste0(myd, "/All.RData"))     # All Data
 
-load(paste0(myd, "/dist_mat1.RData"))  # Trait distance matrix
-load(paste0(myd, "/tt.RData"))
+load(paste0(myd, "/dist_mat1.RData"))  # Species x species distance matrix
+load(paste0(myd, "/tt.RData"))         # Species x trait matrix
 
 # Format community data: -----------------------------------------------------------------
-# Site-species matrix
-l <- list("HNB"=HNB, "ContAll"=ContAll)
-l <- lapply(l, function(x) {rownames(x) <- x$SiteNameE;x
-                            within(x, rm(SiteNameE, DrainageBasinE))
-                            as.matrix(x)}) #OK
+HNB$Period <- rep("HNB", nrow(HNB))
+ContAll$Period <- rep("ContAll", nrow(ContAll))
 
-rownames(All) <- paste0(All$SiteNameE,"_", All$Period)
-rownames(All) <- gsub(" ", "_", rownames(All))
-All <- within(All, rm(SiteNameE, DrainageBasinE, Period))
-All <- as.matrix(All)
-
-
-colnames(All)[colnames(All) == "Agonostomus monticola"] <- "Dajaus monticola"
-All <- All[,order(colnames(All))]
+dt <- bind_rows(HNB, ContAll) # OK
+dt[is.na(dt)] <- 0
+rownames(dt) <- paste0(dt$SiteNameE,"_", dt$Period)
+dt <- within(dt, rm(SiteNameE, DrainageBasinE, Period))
+dt <- dt[rowSums(dt)>0,] # 120 (-14, OK)
+dt <- as.matrix(dt)
+colnames(dt)[colnames(dt) == "Agonostomus monticola"] <- "Dajaus monticola"
+dt <- dt[,order(colnames(dt))]
 dist_mat1 <- dist_mat1[order(rownames(dist_mat1)), order(colnames(dist_mat1))]
 
 
 tt <- tt[order(rownames(tt)),]
-identical(colnames(All), colnames(dist_mat1))  # TRUE
-identical(colnames(All), rownames(tt))         # TRUE
+identical(colnames(dt), colnames(dist_mat1))  # TRUE
+identical(colnames(dt), rownames(tt))         # TRUE
 
 ###########################################################################################
-# Uniqueness (regional):------------------------------------------------------------------
-Uii <- uniqueness(All, dist_matrix = dist_mat1)
+# Trait rarity measures:-------------------------------------------------------------------
 
-#Ui_dim <- uniqueness_dimensions(All, 
-#                                tt,
-#                               metric="euclidean")
+Uii <- uniqueness(dt, dist_matrix = dist_mat1) # uniqueness
+Uii$Ui <- (Uii$Ui-min(Uii$Ui))/(max(Uii$Ui)-min(Uii$Ui)) # normalise
+Ui_dim <- uniqueness_dimensions(dt, 
+                                tt,
+                                metric="euclidean") 
 
 
-# Native/Introduced/Remaining
+Dii <- distinctiveness(dt, dist_matrix = dist_mat1) #distinctivenss
+Dii <- as.data.frame(Dii)
+Di_dim <- distinctiveness_dimensions(dt, 
+                                     tt,
+                                     metric="euclidean") 
+
+Dii$Period <- rep(NA, nrow(Dii))
+Dii$Period <- ifelse(rownames(Dii) %like% "HNB", "Historical", Dii$Period)
+Dii$Period <- ifelse(rownames(Dii) %like% "ContAll", "Current", Dii$Period)
+sum(is.na(Dii$Period))
+Dii <- gather(Dii, key="Species", value="Di", -Period)
+Dii <- Dii[!is.na(Dii$Di),]
+length(unique(Dii$Species)) #99
+setdiff(unique(colnames(dt)), unique(Dii$Species))
+
+# "Gambusia yucatana" (it means, this species only appears in coms of richness = 1)
+# Distinctiveness of this taxon cannot be calculated.
+
+
+###########################################################################################
+# Period & status -------------------------------------------------------------------------
 nat <- sort(unique(names(ContN[,-c(1:2)]))) # 48
 nat[nat=="Agonostomus monticola"] <-"Dajaus monticola"
 ext <- sort(unique(setdiff(names(HNB), names(ContN)))) # 30
@@ -68,89 +86,108 @@ int <- sort(unique(setdiff(names(ContE), names(HNB)))) # 22 (translocated specie
 nat <- c(nat, "Chapalichthys encaustus")
 ext <- setdiff(ext, "Chapalichthys encaustus")
 
-# All traits:
 Uii$Status <- rep(NA, nrow(Uii))
 Uii$Status <- ifelse(Uii$species %in% nat, "Native Remaining", Uii$Status)
 Uii$Status <- ifelse(Uii$species %in% ext, "Extirpated", Uii$Status)
 Uii$Status <- ifelse(Uii$species %in% int, "Introduced", Uii$Status)
 sum(is.na(Uii$Status))
-str(Uii)
 Uii$Status <- as.factor(Uii$Status)
-Uii$species <- paste0(substr(Uii$species,1,1), "_",str_split_fixed(Uii$species, " ", 2)[,2])
-
-
-# Plots:
-#(p1 <- ggplot(Uii, aes(x=Ui, y=reorder(species, Ui))) + 
-# geom_point(stat='identity', aes(col=Status), size=4, alpha=0.7)+
-#  labs(x="Uniqueness", y="Species")+
-#  scale_color_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
-#  theme(axis.text = element_text(size = 30))+
-# theme_bw())
-
-Uii$Ui <- (Uii$Ui-min(Uii$Ui))/(max(Uii$Ui)-min(Uii$Ui))
-
-(p1bp <- ggplot(Uii,
-                aes(x = Status, y = Ui, fill=Status)) +
-                geom_violin(alpha=0.3) +
-                geom_boxplot(alpha=0.5, width=0.1)+
-                xlab("Status") +
-                ylab("Uniqueness") +
-                scale_fill_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
-                labs(title = "Regional-level trait rarity (=Uniqueness)")+
-                theme_bw())
-
-ggsave(p1bp, file= paste0(plot_dir, "/UniquenessAllSpeciesBP.jpg"), width = 9, height = 6)
-
-###########################################################################################
-# Distinctiveness (local):-----------------------------------------------------------------
-Dii <- distinctiveness(All, dist_matrix = dist_mat1)
-Dii <- as.data.frame(Dii)
-
-Dii$Period <- rep(NA, nrow(Dii))
-Dii$Period <- ifelse(rownames(Dii) %like% "HNC", "A) Historical Conservative", Dii$Period)
-Dii$Period <- ifelse(rownames(Dii) %like% "HNB", "B) Historical Broad", Dii$Period)
-Dii$Period <- ifelse(rownames(Dii) %like% "ContN", "C) Current Natives", Dii$Period)
-Dii$Period <- ifelse(rownames(Dii) %like% "ContAll", "D) Current Natives + Exotics", Dii$Period)
-
-Dii <- gather(Dii, key="Species", value="Di", -Period)
-Dii <- Dii[!is.na(Dii$Di),]
 
 Dii$Status <- rep(NA, nrow(Dii))
 Dii$Status <- ifelse(Dii$Species %in% nat, "Native Remaining", Dii$Status)
 Dii$Status <- ifelse(Dii$Species %in% ext, "Extirpated", Dii$Status)
 Dii$Status <- ifelse(Dii$Species %in% int, "Introduced", Dii$Status)
 
-Dii$Species <- as.factor(Dii$Species)
-Dii$Period <- as.factor(Dii$Period)
-Dii$Status <- as.factor(Dii$Status)
-Dii$Species <- paste0(substr(Dii$Species,1,1), "_",str_split_fixed(Dii$Species, " ", 2)[,2])
+#Dii$Period <- as.factor(Dii$Period)
+#Dii$Status <- as.factor(Dii$Status)
+#Dii$Species <- paste0(substr(Dii$Species,1,1), "_",str_split_fixed(Dii$Species, " ", 2)[,2])
 
-DiiII <- subset(Dii, Dii$Period %in% c("B) Historical Broad", "D) Current Natives + Exotics"))
+###########################################################################################
+# Plots -----------------------------------------------------------------------------------
 
-(pDii1 <- ggplot(DiiII, aes(y = Di, x = Species, color=Period, fill=Period)) +
-    geom_boxplot(aes(fill=Period))+
-    xlab("Species") +
-    ylab("Distinctiveness") +
-    labs(title = "Local-level species' trait rarity (=Distinctiveness)")+
-    theme_classic()+
-    theme(axis.text.x = element_blank(),
-          axis.ticks.x = element_blank())+
-    scale_fill_manual(values=c("#7CAE00", "#C77CFF"))+
-    scale_color_manual(values=c("#7CAE00", "#C77CFF"))+
-    facet_wrap(~Status))
+# Violin plots:
 
-(pDii2 <- ggplot(Dii, aes(y = Di, x = Period, fill=Status, color=Status)) +
-    geom_violin(alpha=0.3) +
-    geom_boxplot(width=0.1, position=position_dodge(1))+
+(uviolin <- ggplot(Uii,aes(x = Status, y = Ui, fill=Status)) +
+   geom_point(alpha=0.3)+
+   geom_violin(alpha=0.4) +
+   geom_boxplot(alpha=0.5, width=0.1, position=position_dodge(1))+
+   xlab("Status") +
+   ylab("Uniqueness") +
+   scale_fill_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
+   theme_bw())
+
+(dviolin <- ggplot(Dii, aes(y = Di, x = Period, fill=Status, color=Status)) +
+    geom_point(alpha=0.05, position=position_dodge(1))+
+    geom_violin(alpha=0.4) +
+    geom_boxplot(alpha=0.5, width=0.1, position=position_dodge(1))+
     xlab("Period") +
     ylab("Distinctiveness") +
-    labs(title = "Local-level trait rarity (=Distinctiveness)")+
     scale_fill_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
     scale_color_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
     theme_bw())
 
-ggsave(pDii1, file= paste0(plot_dir, "/DistinctivenessAllSpecies.jpg"), width = 12, height =7) 
-ggsave(pDii2, file= paste0(plot_dir, "/DistinctivenessAllSpecies2.jpg"), width = 10, height = 7) 
+Dii$StatusII <- ifelse((Dii$Period=="Historical" & Dii$Status =="Native Remaining"),
+                     "A) Native Remaining Historical", 
+                     Dii$Status)
+Dii$StatusII <- recode_factor(as.factor(Dii$StatusI), 
+                              "A) Native Remaining Historical"="A) Native Remaining Historical",
+                              "Native Remaining" = "B) Native Remaining Current",
+                              "Extirpated"= "C) Extirpated",
+                              "Introduced" = "D) Introduced")
+
+(dviolinII <- ggplot(Dii,aes(x = StatusII, y = Di, fill=StatusII)) +
+    geom_point(alpha=0.3)+
+    geom_violin(alpha=0.4) +
+    geom_boxplot(alpha=0.5, width=0.1, position=position_dodge(1))+
+    xlab("StatusII") +
+    ylab("Distinctiveness") +
+    scale_fill_manual(values=c("Lightgray", "Darkgray", "#0072B2", "#F0E442"))+
+    theme_bw())
+
+
+# Point plots:
+
+#(upoint <- ggplot(Uii, aes(x=Ui, y=reorder(species, Ui))) + 
+# geom_point(stat='identity', aes(col=Status), size=2, alpha=0.7)+
+# labs(x="Uniqueness", y="Species")+
+# theme(axis.text.y = element_blank())+
+# scale_color_manual(values=c("#0072B2", "#F0E442", "Darkgray"))+
+# theme_bw())
+
+sum(is.na(Dii$Di)) #0
+length(sort(unique(Dii$Species))) # 99 
+
+
+Dii_m <- Dii %>%
+  group_by(Species, StatusII, Period) %>%
+  summarise(Di_m=mean(Di))
+names(Dii_m)[names(Dii_m)=="Species"] <- "species"
+length(unique(Dii_m$species))
+
+merged <- full_join(Dii_m, Uii, by=c("species"))
+
+merged <- merged[!is.na(merged$Di_m),]
+merged$Di_m <- (merged$Di_m - min(merged$Di_m))/(max(merged$Di_m)-min(merged$Di_m)) # normalise
+range(merged$Di_m)
+
+
+(UivsDi <- ggplot(merged, aes(x=Di_m , y=Ui, color=Status))+
+  geom_point()+
+  stat_smooth()+
+  facet_wrap(~Period))
+
+# Boxplot distinctiveness (color Period):
+
+(PeriodDii <- ggplot(Dii, aes(x=Species , y=Di, color=Period))+
+    geom_boxplot()+
+    theme(axis.text.x = element_blank())+
+    theme_classic()+
+    facet_wrap(~Status))
+
+###########################################################################################
+# Save plots: -----------------------------------------------------------------------------
+
+
 
 ###########################################################################################
 # End of script ###########################################################################
